8c3eaf25e836ca4416e587852956439f97d4d3a8
Natalie <snnatalies@gmail.com>
2013-04-08 23:22:30 +0400

    убрала лишнее
    убрала лишние файлы и комментарии

--- helloW/helloW/Website/MyControl.fs
+++ helloW/helloW/Website/MyControl.fs
@@ -9,53 +9,36 @@ module Manager =
 
     module Server = 
         [<Rpc>]
-    //    
-    //    let count questionNumber chosenValue = if (questionNumber % 2 = true) then (chosenValue - 1) else (5 - chosenValue)
         let status () : List<string * int> = 
                     [
                         "Ученик", 1 
                         "Студент", 2
                         "Учитель", 3
                      ];
-        let fff = 2;
-    
 
     module Client = 
         [<JavaScript>]
         //let task = "Напишите программу, при исполнении которой робот остановится в 10 см от стены" 
         let TasksFormlet () = 
-//            let fc = {
-//                Enhance.FormContainerConfiguration.Default with
-//                    Header = "Анкетирование" |> Enhance.FormPart.Text |> Some
-//                    Description = "Анкета для исследования юзабилити проекта QReal:Robots"
-//                        |> Enhance.FormPart.Text
-//                        |> Some
-//            }
+
             let TaskWindow = 
                 Formlet.Sequence [
                     Controls.RadioButtonGroup None ["Получилось!", 0; "Не получилось", 1] 
                     |> Enhance.WithLegend "Напишите программу, при исполнении которой робот остановится в 10 см от стены"
                     |> Enhance.WithTextLabel "Задание"
                 ]
-//                |> Enhance.WithCustomFormContainer {
-//                       fc with Description = "Step 1 - Name" |> Enhance.FormPart.Text |> Some
-//                       }
+
             let ChoiceWindow = 
                 Formlet.Sequence [ 
                     Controls.Button "Попробовать другую задачу."
                     Controls.Button "Перейти к анкете."
                 ] |> Formlet.Horizontal
-                  //|> Enhance.WithFormContainer
-//                  |> Enhance.WithCustomFormContainer {
-//                       fc with Description = "Step 1 - Name" |> Enhance.FormPart.Text |> Some
-//                       }
+ 
             let StateForm =
                 let stat = Server.status () 
                 Formlet.Controls.Select 0 stat 
                 |> Enhance.WithTextLabel "Выберите Ваш статус"
-//                |> Enhance.WithCustomFormContainer {
-//                       fc with Description = "Step 1 - Name" |> Enhance.FormPart.Text |> Some
-////                       }
+
             let QestionnaireFormlet =
                 Formlet.Sequence [
                     Controls.RadioButtonGroup None ["1", "1"; "2", "2"; "3", "3"; "4", "4";"5", "5"] 
@@ -63,24 +46,12 @@ module Manager =
                     Controls.RadioButtonGroup None ["1", "1"; "2", "2"; "3", "3"; "4", "4";"5", "5"] 
                     |> Enhance.WithLegend "2) Программа слишком сложная."
                 ]
-//                |> Enhance.WithCustomFormContainer {
-//                       fc with Description = "Step 1 - Name" |> Enhance.FormPart.Text |> Some
-//                       }
-//      
-
 
             Formlet.Yield (fun _ _ _ _ -> "hello")
             <*> StateForm
             <*> TaskWindow
             <*> ChoiceWindow
             <*> QestionnaireFormlet
-//            Formlet.Do {
-//                 return! 
-//                    Formlet.OfElement (fun () -> Div [Text "Спасибо за Ваши ответы!"])
-//                
-//                }
-//                |> Formlet.Flowlet  
-
 
     [<JavaScript>]
     let Main () =
@@ -100,11 +71,7 @@ module Manager =
 type MyControl() =
     inherit Web.Control()
     [<JavaScript>]
-//    override this.Body = Manager.Client.TasksFormlet () :> Html.IPagelet
-
-
-
-    override this.Body = Manager.Main () // :> Html.IPagelet
+    override this.Body = Manager.Main () 
     
 
 
